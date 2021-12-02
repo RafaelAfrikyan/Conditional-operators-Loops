b40a10004102e85c2a895d171333d5cccd619b4d
RafaelAfrikyan <rafaelafrikyan95@gmail.com>
2021-12-02 12:53:12 +0400

    count angels

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ while (i < numbers.length) {
  }
   i++;
 }
+
+//2. . Given two variables, which are the angles of a triangle. Find the third angle of the triangle. (Sum of the angles of a triangle equals 180 degrees).
+
+let angels = [30, 30];
+let sumOfAngels = 0;
+let sum = 180;
+let i;
+let result;
+for (i = 0; i < angels.length; i++){
+sumOfAngels = sumOfAngels + angels[i]
+}
+result = sum - sumOfAngels;
+console.log(result)
